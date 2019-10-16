232d66d655b4aa7c4b84e36e4503a7af6926d06f
josebroldant@gmail.com <josebroldant@gmail.com>
2019-10-16 16:36:25 -0500

    added hcsr04

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,11 @@
 //#include <WiFi101.h> // for WiFi 101 shield or MKR1000
 #include <WiFiUdp.h>
 
+#include <HCSR04.h>
+
+UltraSonicDistanceSensor distanceSensor(13, 12);  // Initialize sensor that uses digital pins 13 and 12.
+
+
 WiFiUDP ntpUDP;
 
 const char *ssid     = "jose";
@@ -32,5 +37,9 @@ void loop() {
 
   Serial.println(timeClient.getFormattedTime());
 
+  Serial.println(distanceSensor.measureDistanceCm());
+
   delay(1000);
+
+  
 }
