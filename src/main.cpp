6b88bc4445f65f613af90fb10a7c4c5ffefc28d1
josebroldant@gmail.com <josebroldant@gmail.com>
2019-10-16 14:32:36 -0500

    added wifi client

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
-#include <Arduino.h>
+#include <NTPClient.h>
+// change next line to use with another board/shield
+#include <ESP8266WiFi.h>
+//#include <WiFi.h> // for WiFi shield
+//#include <WiFi101.h> // for WiFi 101 shield or MKR1000
+#include <WiFiUdp.h>
 
-void setup() {
-  // put your setup code here, to run once:
+WiFiUDP ntpUDP;
+
+const char *ssid     = "jose";
+const char *password = "<1234567890>";
+
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+
+void setup(){
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  while ( WiFi.status() != WL_CONNECTED ) {
+    delay ( 500 );
+    Serial.print ( "." );
+  }
+
+  timeClient.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //Aqui en el loop hacer el post constante de las variables.
+  timeClient.update();
+
+  Serial.println(timeClient.getFormattedTime());
+
+  delay(1000);
 }
