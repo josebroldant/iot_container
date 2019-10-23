83f7ba1d1a73ccf373c74209a4437ff4fb27a9c8
josebroldant@gmail.com <josebroldant@gmail.com>
2019-10-23 16:42:40 -0500

    updated server connection

--- src/main.cpp
+++ src/main.cpp
@@ -110,14 +110,11 @@ void loop() {
 
   //VERIFICACION DE LA CONEXION
 
-  if (!client.connect(host, port))
-  {
-    Serial.println("conecction failed");
-    delay(1000);
-  }
+  //Conexion al servidor
 
-  //Conexion a mongoDB
-  if(client.available()>0){
+  String url = "localhost:3000/index";
+
+  while(client.available()){
       Serial.println("Disponible");
       String line = client.readStringUntil('\n');
       Serial.println(line);
@@ -125,8 +122,29 @@ void loop() {
       if(!root.success()){
         Serial.println("parseObject() failed");
       }
-    }
+      Serial.print("Requesting POST: ");
+      // Send request to the server:
+      client.println("POST / HTTP/1.1");
+      client.println("Host: server_name");
+      client.println("Accept: */*");
+      client.println("Content-Type: application/x-www-form-urlencoded");
+      client.print("Content-Length: ");
+      client.println(line);
+      client.println();
+      // This will send the request to the server
+      //this is a get method working
+      client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+           "Connection: close\r\n\r\n");
+      unsigned long timeout = millis();
+      while (client.available() == 0) {
+        if (millis() - timeout > 5000) {
+          Serial.println(">>> Client Timeout !");
+          client.stop();
+        return;
+        }
+      }
 
   //Post de las variables
   
+  }
 }
